ac25ee534fadf5cd56362532461a7b757d66cfa4
Lukas <lukasrs13@gmail.com>
2025-01-27 16:06:31 -0300

    update-27-01-25

---
+++ uc1/aula04/ex01/scripts.js
@@ -0,0 +1,34 @@
+um = "Segunda Feira"
+dois = "Terça Feira"
+tres = "Quarta Feira"
+quatro = "Quinta Feira"
+cinco = "Sexta-Feira"
+seis = "Sábado"
+sete = "Domingo"
+const dia = 5
+
+switch(dia){
+    case 1:
+        console.log(um);
+        break;
+    case 2:
+            console.log(dois);
+            break;
+    case 3:
+        console.log(tres);
+        break;
+    case 4:
+            console.log(quatro);
+            break;
+    case 5:
+        console.log(cinco);
+        break;
+    case 6:
+            console.log(seis);
+            break;
+    case 7:
+        console.log(sete);
+        break;
+    default:
+        console.log("Dia Inválido")
+    } 
